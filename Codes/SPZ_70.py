446ba6580c68716363ee889eb8c64fde6ed738b1
Aniket Sanghi <34240757+cosmicoder@users.noreply.github.com>
2020-07-10 22:33:46 -0500

    Update SPZ_70.py

--- Codes/SPZ_70.py
+++ Codes/SPZ_70.py
@@ -7,8 +7,8 @@ import math
 ofile = open('SPITZER_70_match4.txt', mode='w')
 
 # Loading csv data into program references
-spitzer = np.loadtxt('SPITZER_70_reduced.csv', delimiter=',')
-gmrt = np.loadtxt('GMRT_400MHz_catalog_reduced.csv', delimiter=',')
+spitzer = np.loadtxt('CSV_Files/SPITZER_70_reduced.csv', delimiter=',')
+gmrt = np.loadtxt('CSV_Files/GMRT_400MHz_catalog_reduced.csv', delimiter=',')
 
 # Linear Search Algorithm on NumPy Array
 count = 1
